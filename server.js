f7a819125516bfbebbb39ba8401c12460c81184a
Sai Kumar <Sai.Kumar4@ibm.com>
2021-10-19 17:46:27 +0530

    enhancements

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const path = require('path');
-const users = require('./public/users/users.json')
 const { AppConfiguration } = require('ibm-appconfiguration-node-sdk'); // App Configuration SDK require
+const users = require('./public/users/users.json');
 
 const app = express();
 const port = 3000;
@@ -13,6 +13,7 @@ let userDetails = {};
 // view engine setup
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
+
 app.set('view engine', 'html');
 app.use(session({ secret: 'work hard', saveUninitialized: true, resave: true }));
 app.use(express.json());
@@ -25,8 +26,8 @@ app.use(express.static(path.join(__dirname, 'public', 'users')));
 const region = process.env.REGION; // use `us-south` for Dallas. `eu-gb` for London. au-syd for Sydney
 const guid = process.env.GUID;
 const apikey = process.env.APIKEY;
-const collectionId = process.env.COLLECTION_ID
-const environmentId = process.env.ENVIRONMENT_ID
+const collectionId = process.env.COLLECTION_ID;
+const environmentId = process.env.ENVIRONMENT_ID;
 
 // App Configuration SDK initialisation
 const client = AppConfiguration.getInstance();
@@ -41,15 +42,14 @@ let primeItems;
 
 function loginCheck(req, res, next) {
   if (req.session && req.session.userEmail) {
-    req.isLoggedInUser = true
-    userDetails = users.data.filter((e) => e.email === req.session.userEmail)[0]
+    req.isLoggedInUser = true;
+    userDetails = users.data.filter((e) => e.email === req.session.userEmail)[0];
   } else {
-    req.isLoggedInUser = false
+    req.isLoggedInUser = false;
   }
-  next()
+  next();
 }
 function configCheck(req, res, next) {
-
   const bluetootEarphonesFeature = client.getFeature('bluetooth-earphones');
   bluetoothEarphones = bluetootEarphonesFeature.isEnabled();
 
@@ -58,28 +58,33 @@ function configCheck(req, res, next) {
 
   const entityId = userDetails.email ? userDetails.email : 'defaultUser';
   const entityAttributesForPrimeItems = {
-    is_prime: userDetails.is_prime
+    is_prime: userDetails.is_prime,
   };
   const primeItemsFeature = client.getFeature('exclusive-offers-section');
-  primeItems = primeItemsFeature.getCurrentValue(entityId, entityAttributesForPrimeItems)
+  primeItems = primeItemsFeature.getCurrentValue(entityId, entityAttributesForPrimeItems);
 
   next();
 }
 
 app.get('/', [loginCheck, configCheck], (req, res) => {
-  res.render('index.html', { isLoggedInUser: req.isLoggedInUser, userEmail: req.session.userEmail, flashSaleBanner, bluetoothEarphones, primeItems });
+  res.render('index.html', {
+    isLoggedInUser: req.isLoggedInUser,
+    userEmail: req.session.userEmail,
+    flashSaleBanner,
+    bluetoothEarphones,
+    primeItems,
+  });
 });
 
 app.post('/', (req, res, next) => {
   if (req.body.logemail && req.body.logpassword) {
     req.session.userEmail = req.body.logemail;
     return res.redirect('/');
-  } else {
-    var err = new Error('All fields required.');
-    err.status = 400;
-    return next(err);
   }
-})
+  const err = new Error('All fields required.');
+  err.status = 400;
+  return next(err);
+});
 
 app.get('/logout', (req, res, next) => {
   if (req.session) {
@@ -95,7 +100,7 @@ app.get('/logout', (req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500).send('<h2>Error</h2>' + err)
+  res.status(err.status || 500).send(`<h2>Error</h2>${err}`);
 });
 
 app.listen(port, () => {
